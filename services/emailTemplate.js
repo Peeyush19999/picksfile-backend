a96992b0c7ef5e54b2436cc4d8815748ad4634a1
Peeyush Dangi <37629008+Peeyush19999@users.noreply.github.com>
2022-11-06 15:06:57 +0530

    Update emailTemplate.js

--- services/emailTemplate.js
+++ services/emailTemplate.js
@@ -148,7 +148,7 @@ module.exports = ({ emailFrom, downloadLink, size, expires }) => {
                             <tr>
                             <td class="content-block" style="font-family: sans-serif; vertical-align: top; padding-bottom: 10px; padding-top: 10px; font-size: 12px; color: #999999; text-align: center;">
                                 
-                                <br> Want to share a file? <a href="http://localhost:3000" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">Picksfile</a>.
+                                <br> Want to share a file? <a href="https://picksfile.netlify.app" style="text-decoration: underline; color: #999999; font-size: 12px; text-align: center;">Picksfile</a>.
                             </td>
                             </tr>
                             <tr>
